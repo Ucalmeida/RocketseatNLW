c9de7504bfab0a4724575cec8dbe73db0a0d876a
93118180544 <urian.castro@gmail.com>
2021-06-26 11:38:36 -0300

    Commit inicial NLW

---
+++ src/database/index.ts
@@ -0,0 +1,5 @@
+import { createConnection } from 'typeorm';
+
+// Chamar o createConnection
+createConnection(); // Isso vai fazer com que o banco seja inicializado. 
+                    // Com que ele crie uma conexão com o banco de dados.
